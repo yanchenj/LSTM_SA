9b35b0abcdc1ba388e493f7a15009d50288cbacb
Wenching Tsai <wenctsai@umich.edu>
2017-11-30 21:00:06 -0500

    modified preprocessing

--- preprocess.py
+++ preprocess.py
@@ -3,78 +3,74 @@ import numpy as np
 from random import *
 from gensim.models import Word2Vec
 from nltk.corpus import movie_reviews
-			
+
+
 def preprocess():
-	pos_data, neg_data = [],[]
-	print 'start loading data'
-	path = os.getcwd()+"/aclImdb/train/pos/"
-	for filename in os.listdir(path):
-		pos_file = open("aclImdb/train/pos/"+filename, 'r')
-		pos_data = pos_data + pos_file.readlines()
-		pos_file.close()
-
-	path = os.getcwd()+"/aclImdb/train/neg/"
-	for filename in os.listdir(path):
-		neg_file = open("aclImdb/train/neg/"+filename, 'r')
-		neg_data = neg_data + neg_file.readlines()
-		neg_file.close()
-
-	p_idx, n_idx = 0,0
-	p_len, n_len = len(pos_data), len(neg_data)
-
 	train_data, train_label = [],[]
 	model = Word2Vec(movie_reviews.sents())
 
-	while p_idx < p_len and n_idx < n_len:
-		if randint(0,1) == 0:
-			for word in neg_data[n_idx].split():
+	path = os.getcwd()+"/aclImdb/train/small_pos/"
+	for filename in os.listdir(path):
+		pos_file = open("aclImdb/train/small_pos/"+filename, 'r')
+		for line in pos_file.readlines():
+			review = []
+			for word in line.split():
 				if word in model.wv.vocab:
-					train_data.append(model.wv[word])
-					train_label.append(0)
-			n_idx = n_idx + 1
-		else:
-			for word in pos_data[p_idx].split():
-				if word in model.wv.vocab:	
-					train_data.append(model.wv[word])
-					train_label.append(1)
-			p_idx = p_idx + 1
+					review.append(model.wv[word])
+			train_data.append(review)
+			train_label.append((0,1))
+		pos_file.close()
 
-	if p_idx < p_len:
-		for i in xrange(p_idx,p_len):
-			for word in pos_data[i].split():
+	path = os.getcwd()+"/aclImdb/train/small_neg/"
+	for filename in os.listdir(path):
+		neg_file = open("aclImdb/train/small_neg/"+filename, 'r')
+		for line in neg_file.readlines():
+			review = []
+			for word in line.split():
 				if word in model.wv.vocab:
-					train_data.append(model.wv[word])
-					train_label.append(1)
-	if n_idx < n_len:
-		for i in xrange(n_idx,n_len):
-			for word in neg_data[i].split():
-				if word in model.wv.vocab:
-					train_data.append(model.wv[word])
-					train_label.append(0)
-	np.savetxt('train_data.txt',train_data)
+					review.append(model.wv[word])
+			train_data.append(review)
+			train_label.append((1,0))
+		neg_file.close()
+
+	print len(train_data),len(train_label)
+	x_train = np.array([np.array(xi) for xi in train_data])
+	y_train = np.array(train_label)
+	print x_train.shape, x_train[0].shape, x_train[1].shape
+	print y_train.shape, y_train[0].shape, y_train[1].shape
+	np.save('train_data.npy', x_train)
+	np.save('train_label.npy', y_train)
 
 	test_data, test_label = [],[]
 
-	path = os.getcwd()+"/aclImdb/test/pos/"
+	path = os.getcwd()+"/aclImdb/test/small_pos/"
 	for filename in os.listdir(path):
-		pos_file = open("aclImdb/test/pos/"+filename, 'r')
+		pos_file = open("aclImdb/test/small_pos/"+filename, 'r')
 		for line in pos_file.readlines():
+			review = []
 			for word in line.split():
 				if word in model.wv.vocab:
-					test_data.append(model.wv[word])
-					test_label.append(1)
+					review.append(model.wv[word])
+			test_data.append(review)
+			test_label.append((0,1))
 		pos_file.close()
 
-	path = os.getcwd()+"/aclImdb/test/neg/"
+	path = os.getcwd()+"/aclImdb/test/small_neg/"
 	for filename in os.listdir(path):
-		neg_file = open("aclImdb/test/neg/"+filename, 'r')
+		neg_file = open("aclImdb/test/small_neg/"+filename, 'r')
 		for line in neg_file.readlines():
+			review = []
 			for word in line.split():
 				if word in model.wv.vocab:
-					test_data.append(model.wv[word])
-					test_label.append(0)
+					review.append(model.wv[word])
+			test_data.append(review)
+			test_label.append((0,1))
 		neg_file.close()
+
 	print 'finish loading data'
-	return [np.asarray(train_data),np.asarray(train_label),np.asarray(test_data),np.asarray(test_label)]
-
-
+	print len(test_data),len(test_label)
+	x_test = np.array([np.array(xi) for xi in test_data])
+	y_test = np.array(test_label)
+	np.save('test_data.npy', x_test)
+	np.save('test_label.npy', y_test)
+	return [np.asarray(x_train),np.asarray(y_train),np.asarray(x_test),np.asarray(y_test)]
